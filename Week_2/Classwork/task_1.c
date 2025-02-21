e327ea8d4a789aec42932ffc3a468d146aabd26c
Sihan <haroonmuhammedsihan@gmail.com>
2025-02-21 20:41:43 +0000

    Exercise added

---
+++ Week_2/Classwork/task_1.c
@@ -0,0 +1,7 @@
+/*
+    If-Statements
+    1. Write a program that:
+        Asks the user how old they are (which the user should enter via the console screen)
+        If the user enters an age that is 0 or less, 
+        display "wow, people start using computers early" on the console screen
+*/
